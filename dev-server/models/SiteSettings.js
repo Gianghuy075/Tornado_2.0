5fe2c5728467801cb2c2cd6ae6d6d42ba858a244
Quang Nguyen <n142@gmail.com>
2026-03-12 18:46:53 +0800

    update

--- dev-server/models/SiteSettings.js
+++
@@ -1,8 +0,0 @@
-/* SiteSettings model - stores global site configuration such as logo */
-const mongoose = require('mongoose');
-
-const siteSettingsSchema = new mongoose.Schema({
-  logoUrl: { type: String, default: "" },
-}, { timestamps: true });
-
-module.exports = mongoose.model('SiteSettings', siteSettingsSchema);
